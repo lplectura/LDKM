5dcbab8df0063fb59dffcc60b3c612ce14663e05
yzel <lplectura@up.edu.ph>
2018-11-07 16:18:26 +0800

    create training data base

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ ruby '2.5.3'
 gem 'rails', '~> 5.2.1'
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3'
-gem 'pg'
+gem 'pg', '>=0.18', '<1.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_07_055225) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "trainings", force: :cascade do |t|
     t.string "name"
     t.date "datefrom"
